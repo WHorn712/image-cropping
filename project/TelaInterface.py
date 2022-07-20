af88dc95398834b8b42cf3f0f3561448a4d64793
Wellington Augusto Horn <wellingtondeveloper31@gmail.com>
2022-07-20 18:35:18 -0300

    update 20-7-2022 2

--- project/TelaInterface.py
+++ project/TelaInterface.py
@@ -7,9 +7,7 @@ def open_tela():
     janela.geometry("400x400")
     put_title(janela)
 
-
-    bt = Button(janela, text="aperte")
-    bt.grid(column=2, row=2)
+    put_buttleCuple(janela)
 
     janela.mainloop()
 
@@ -27,3 +25,13 @@ def put_title(janela):
         listaLabels.append(Label(janela, text=""))
         listaLabels[i].grid(column=i + 1, row=1)
         i += 1
+
+def put_buttleCuple(janela):
+    photo = PhotoImage(file=r'C:\Users\Micro\OneDrive\Documentos\Git\image-cropping\project\IMAGENS Program\buttonImage.png')
+    photo = photo.subsample(5,5)
+    bt = Button(janela, image=photo)
+    bt.grid(column=2,row=2)
+    """photo = PhotoImage(file=r'C:\Gfg\circle.png')
+    Button(janela, text='Click Me !', image=photo).pack(side=TOP)
+    quantidade = 50
+    bt = Button(janela, )"""
