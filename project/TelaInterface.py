6127416fd8a651b3a3fda44299dbdd0f3101cb2b
Wellington Augusto Horn <wellingtondeveloper31@gmail.com>
2022-07-20 12:54:04 -0300

    update 20-7-2022 1

--- project/TelaInterface.py
+++ project/TelaInterface.py
@@ -5,12 +5,25 @@ import tkinter.filedialog as fd
 def open_tela():
     janela = Tk()
     janela.geometry("400x400")
-
-    texto = Label(janela, text ="CORTAR IMAGEM")
-    texto.grid(column=0, row=0)
+    put_title(janela)
 
 
-
-
+    bt = Button(janela, text="aperte")
+    bt.grid(column=2, row=2)
 
     janela.mainloop()
+
+def put_title(janela):
+    janela.title("CORTAR IMAGEM")
+
+    quantidade = 12
+    texto = Label(janela, text="CORTAR IMAGEM")
+    texto.config(font=("Arial", 15))
+    texto.grid(column=quantidade, row=1)
+
+    listaLabels = []
+    i = 0
+    while i < quantidade:
+        listaLabels.append(Label(janela, text=""))
+        listaLabels[i].grid(column=i + 1, row=1)
+        i += 1
--- project/main.py
+++ project/main.py
@@ -1,11 +1,12 @@
 import folder
 import code_crup
+import TelaInterface
 
-fol_origem = folder.get_folder_origem()
-fol_destino = folder.get_folder_destination()
+#fol_origem = folder.get_folder_origem()
+#fol_destino = folder.get_folder_destination()
 
-code_crup.save_images_cutted_in_path(folder_origem=fol_origem, folder_destino=fol_destino)
-
+#code_crup.save_images_cutted_in_path(folder_origem=fol_origem, folder_destino=fol_destino)
+TelaInterface.open_tela()
 
 
 
